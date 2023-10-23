729b4d65fc080074d9d7d2c7ffea09fa16ec1f9d
Bart Smyth <bart.smyth@maxmine.com.au>
2023-10-24 09:23:16 +1030

    turn off arrows

--- nvim/.config/nvim/lua/config/remaps.lua
+++ nvim/.config/nvim/lua/config/remaps.lua
@@ -2,10 +2,15 @@ local km = vim.keymap
 
 km.set('i', 'jk', '<Esc>')
 km.set('i', 'kj', '<Esc>')
-
 km.set('v', 'jk', '<Esc>')
 km.set('v', 'kj', '<Esc>')
 
 km.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>")
 
 km.set("n", "<leader>qq", "<cmd>qa<cr>")
+
+-- no cheating
+km.set({ "i", "x", "n", "s" }, "<Up>", "<Noop>")
+km.set({ "i", "x", "n", "s" }, "<Down>", "<Noop>")
+km.set({ "i", "x", "n", "s" }, "<Right>", "<Noop>")
+km.set({ "i", "x", "n", "s" }, "<Left>", "<Noop>")
