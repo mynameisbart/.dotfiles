5bbf01132d8ea224c76655eda3ee5621b23a0f88
Bart Smyth <bart.smyth@maxmine.com.au>
2023-10-23 22:10:17 +1030

    icons for nvim, fix truecolours inside of tmux

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -116,7 +116,7 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
